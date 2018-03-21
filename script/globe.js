5992f8f6847af53a2e045a7cbba1679096267c6f
duncangeere <radio.edit@gmail.com>
2018-03-21 08:44:13 +0100

    Fixing globe markers

--- script/globe.js
+++ script/globe.js
@@ -6,7 +6,7 @@ let initialize = () => {
     // Plot the Earth
     earth = new WE.map('earth_div', {
         zoom: 5,
-        position: [0, 0], // Get user's position from the geolocation API and set as starting position
+        position: [0, 0], // Default start position
     });
 
     // Load the tiles
@@ -17,8 +17,6 @@ let initialize = () => {
     });
     tiles.addTo(earth);
 
-    //earth.flyTo([40.6894, -74.0446]);
-
     // Define the tooltip code
     let tooltip = (language) => `<b>${language.name}</b> - ${language.danger}<br>Speakers: ${language.speakers}<br><em>${language.blurb}</em>`;
 
@@ -26,7 +24,7 @@ let initialize = () => {
     let langPlot = (languages, category, markerImg) => {
         for (let language of languages.filter(lang => lang.danger == category)) {
             // Create the point
-            let point = WE.marker([language.lat, language.lon], markerImg, 10, 10);
+            let point = WE.marker([language.lat, language.lon], markerImg, 20, 20);
             // Add it to the globe
             point.addTo(earth);
             // Give it a tooltip
@@ -48,11 +46,10 @@ let initialize = () => {
             langPlot(languages, "Definitely endangered", "images/def_marker.png");
             langPlot(languages, "Severely endangered", "images/sev_marker.png");
             langPlot(languages, "Critically endangered", "images/cri_marker.png");
-
         });
-
 };
 
+// Update function for resizing
 let update = () => {};
 
 initialize();
